c26221ed88c963c6a0b3ab226a2c63e765577372
JasonFerry <43120122+JasonFerry@users.noreply.github.com>
2018-10-13 11:10:35 -0400

    Adding our Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+       
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+            if (other.gameObject.CompareTag("Pick Up"))
+            {
+                other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+            }
+        if (count == 12)
+        {
+            transform.position = new Vector3(20.0f, 25.0f, 0.0f);
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You won with game with a score of: " + count.ToString();
+        }
+    }
+}
